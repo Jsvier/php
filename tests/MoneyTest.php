12f8b87553db055c8883b19592236778025198fe
Javier <jetchepare@makro.com.ar>
2018-02-10 23:32:13 -0300

    Update MoneyTest.php

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -2,6 +2,7 @@
 /*
  * This file is part of the Money package.
  *
+ 
  * (c) Sebastian Bergmann <sebastian@phpunit.de>
  *
  * For the full copyright and license information, please view the LICENSE
